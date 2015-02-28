87c54a2dd7862773e5f57293dbb6b79ae795770c
totzYuta <yuta.totz@gmail.com>
2015-02-28 14:46:37 -0800

    [modify] Create Sample Map App to Move to Place

--- MapTutorial/map.js
+++ MapTutorial/map.js
@@ -2,43 +2,23 @@
 var dataResults;
 
 function initialize() {
-
+    var latlng = new google.maps.LatLng(35.680865, 139.767036);
+    
     map = new google.maps.Map(document.getElementById('mapdisplay'), {
-        zoom: 3,
-        center: new google.maps.LatLng(40, -187.3),
+        zoom: 13,
+        center: latlng,
         mapTypeId: google.maps.MapTypeId.TERRAIN
     });
 
-    addMarkers();
 
-}
-
-eqfeed_callback = function (results) {
-    dataResults = results;
-}
-
-function addMarkers() {
-    for (var i = 0; i < dataResults.features.length; i++) {
-        var quake = dataResults.features[i];
-        var coors = quake.geometry.coordinates;
-        var latLong = new google.maps.LatLng(coors[1], coors[0]);
-        var marker = new google.maps.Marker({
-            position: latLong,
-            map: map
-            //icon: getCircle(earthquake.properties.mag)
-        })
-    }
-}
-
-function getCircle(magnitude) {
-    return {
-        path: google.maps.SymbolPath.CIRCLE,
-        fillColor: 'red',
-        fillOpacity: .2,
-        scale: Math.pow(2, magnitude) / Math.PI,
-        strokeColor: 'white',
-        strokeWeight: .5
-    };
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+function toTokyo() {
+    map.panTo(new google.maps.LatLng(35.680865, 139.767036));
+}
+
+function toShinbashi() {
+    map.panTo(new google.maps.LatLng(35.666397, 139,758153));
+}
