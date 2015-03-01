7dab251e7df70a8319c7b56ddb399ac1a03b11de
totzYuta <yuta.totz@gmail.com>
2015-02-28 16:22:46 -0800

    [modify] Create map app which show the direction of two points

--- MapTutorial/map.js
+++ MapTutorial/map.js
@@ -1,24 +1,37 @@
-﻿var map;
-var dataResults;
+﻿var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
 
 function initialize() {
-    var latlng = new google.maps.LatLng(35.680865, 139.767036);
-    
-    map = new google.maps.Map(document.getElementById('mapdisplay'), {
-        zoom: 13,
-        center: latlng,
-        mapTypeId: google.maps.MapTypeId.TERRAIN
+
+    directionsDisplay = new google.maps.DirectionsRenderer();
+
+    var mapOptions = {
+        zoom: 7,
+        center: new google.maps.LatLng(41.850033, -87.6500523)
+    };
+
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    directionsDisplay.setMap(map);
+    directionsDisplay.setPanel(document.getElementById('directions-panel'));
+
+    var control = document.getElementById('control');
+    control.style.display = 'block';
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+}
+
+function calcRoute() {
+    var start = document.getElementById('start').value;
+    var end = document.getElementById('end').value;
+    var request = {
+        origin: start,
+        destination: end,
+        travelMode: google.maps.TravelMode.DRIVING
+    };
+    directionsService.route(request, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+        }
     });
-
-
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
-
-function toTokyo() {
-    map.panTo(new google.maps.LatLng(35.680865, 139.767036));
-}
-
-function toShinbashi() {
-    map.panTo(new google.maps.LatLng(35.666397, 139,758153));
-}
